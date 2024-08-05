689af915a2cdb30fa262816c05a7cebea7e4065a
Ahmed Khaled <92623979+a7medk7aledak@users.noreply.github.com>
2024-08-05 17:31:20 +0300

    schema

--- models/customerSchema.js
+++ models/customerSchema.js
@@ -2,15 +2,45 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // define the Schema (the structure of the article)
-const customerSchema = new Schema({
-    FirstName: String,
-    LastName: String,
-    Email: String,
-    Telephone: String,
-    Age: String,
-    Country: String,
-    Gender: String,
-},{timestamps: true});//to make date and time,when add new data it make time to createAt & updateAt that data 
+const customerSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    telephone: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);//to make date and time,when add new data it make time to createAt & updateAt that data 
 
 // Create a model based on that schema
 const Customer = mongoose.model("Customer", customerSchema);//it take Customer and add s to make cluster
